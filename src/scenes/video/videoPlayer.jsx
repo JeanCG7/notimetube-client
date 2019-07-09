91ce1e350c29db71a183c3654697604ecc5c3b36
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-07-09 17:37:04 -0300

    Arrumando link static da url do video

--- src/scenes/video/videoPlayer.jsx
+++ src/scenes/video/videoPlayer.jsx
@@ -19,25 +19,25 @@ export default class VideoPlayer extends Component {
             this.setState({
                 title: resp.data.name,
                 description: resp.data.description,
-                file: resp.data.file
+                file: resp.data.file.replace('api/public/uploads', '/public/uploads')
             })
         })
     }
 
 
     render = () => {
-        const baseUrl = 'D:\\UTFPR\\7 semestre\\Web 2\\projeto kassia-jean-final\\notimetube-api\\';
+        const baseUrl = 'http://localhost:3001';
         return (
             <section as={Col} id="videoBrowse" className="justify-content-center">
                 <Row xs={12} sm={12} md={10} lg={8} xl={6} className="justify-content-center">
                     <h1>{this.state.title}</h1>
-               </Row>
-               <Row xs={12} sm={12} md={10} lg={8} xl={6} className="justify-content-center">
+                </Row>
+                <Row xs={12} sm={12} md={10} lg={8} xl={6} className="justify-content-center">
                     <ReactPlayer id="react-player" url={baseUrl + this.state.file} playing />
-               </Row>
-               <Row xs={12} sm={12} md={10} lg={8} xl={6} className="justify-content-center">
+                </Row>
+                <Row xs={12} sm={12} md={10} lg={8} xl={6} className="justify-content-center">
                     <p>{this.state.description}</p>
-               </Row>
+                </Row>
             </section>
         )
     }
--- src/services/video.js
+++ src/services/video.js
@@ -1,26 +1,15 @@
-import axios from 'axios';
-
-const url = 'http://localhost:3001/videos'
-// const axiosConfig = {
-//     headers: {
-//         'Content-Type': 'application/json;charset=UTF-8',
-//         "Access-Control-Allow-Origin": "*",
-//     }
-//   };
+import api from './api';
 
 export const upload = (video) => {
-  const token = window.localStorage.getItem('token');
-  return axios.post(url, video, { headers: { "Authorization": token, "content-type": 'multipart/form-data' } })
+  return api.post('/videos', video, { headers: { "content-type": 'multipart/form-data' } })
 }
 
 export const get = (id) => {
-  const token = window.localStorage.getItem('token');
-  return axios.get(url + `/${id}`, { headers: { "Authorization": token } });
+  return api.get('/videos' + `/${id}`);
 }
 
 export const getAll = () => {
-  const token = window.localStorage.getItem('token');
-  return axios.get(url, { headers: { "Authorization": token } });
+  return api.get('/videos');
 }
 
 

2071aa20e4fb6643c157bf986283a0f9aedbec17
Jean <jean@forlogic.net>
2019-07-09 01:36:45 -0300

    Updates

---
+++ precache-manifest.1cb003f28fce5df3a3dffca502c14496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc27eacb554851e1eccfa59c1648521",
+    "url": "/notimetube-client/index.html"
+  },
+  {
+    "revision": "5fa2b8e0d4b1ae22cf41",
+    "url": "/notimetube-client/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "099edb83cb838d45181d",
+    "url": "/notimetube-client/static/css/main.86a0c718.chunk.css"
+  },
+  {
+    "revision": "5fa2b8e0d4b1ae22cf41",
+    "url": "/notimetube-client/static/js/2.0e0e2ee3.chunk.js"
+  },
+  {
+    "revision": "099edb83cb838d45181d",
+    "url": "/notimetube-client/static/js/main.7e82dcee.chunk.js"
+  },
+  {
+    "revision": "4aed21df22c87e0bbe9c",
+    "url": "/notimetube-client/static/js/runtime~main.f21f5c17.js"
+  }
+]);
